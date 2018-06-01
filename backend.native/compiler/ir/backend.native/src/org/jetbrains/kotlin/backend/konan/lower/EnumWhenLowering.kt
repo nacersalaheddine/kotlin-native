6bcd6a9aaae514724b4a6e91e7c63d7eb9d0751d
Sergey Bogolepov <ace.is.not.my.card@gmail.com>
2018-06-01 20:16:17 +0700

    wip

--- backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/lower/EnumWhenLowering.kt
+++ backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/lower/EnumWhenLowering.kt
@@ -23,9 +23,7 @@ import org.jetbrains.kotlin.types.isNullable
 // Look for when-constructs where subject is enum entry.
 // Replace branches that are comparisons with compile-time known enum entries
 // with comparisons of ordinals.
-internal class EnumWhenLowering(
-        private val context: Context)
-    : IrElementTransformerVoid(), FileLoweringPass {
+internal class EnumWhenLowering(private val context: Context) : IrElementTransformerVoid(), FileLoweringPass {
 
     private val areEqualByValue = context.ir.symbols.areEqualByValue.first {
         it.owner.valueParameters[0].type == context.builtIns.intType
@@ -66,17 +64,15 @@ internal class EnumWhenLowering(
         // Will be initialized only when we found a branch that compares
         // subject with compile-time known enum entry.
         val ordinalVariable: IrVariable by lazy {
-            val variable = createOrdinalVariable(expression)
+            val variable = createEnumOrdinalVariable(expression)
             expression.statements.add(1, variable)
             variable
         }
         val whenExpr = expression.statements[1] as IrWhen
-        // ordinalVariable should be passed lazily because it should be created
-        // only once.
+        // ordinalVariable should be passed lazily because it should be created only once.
         lowerWhen(whenExpr) { ordinalVariable }
         // Process comma-separated cases.
-        whenExpr.branches
-                .filter(::isCommaSeparatedCondition)
+        whenExpr.branches.filter { isCommaSeparatedCondition(it.condition) }
                 .map { it.condition as IrWhen }
                 .forEach { lowerWhen(it) { ordinalVariable } }
         // Process nested when constructs.
@@ -84,23 +80,26 @@ internal class EnumWhenLowering(
         return expression
     }
 
-    private fun lowerWhen(whenExpr: IrWhen, ordinalVariableProvider: () -> IrVariable) {
-        whenExpr.branches.forEach {
-            it.condition = lowerIfNeeded(it.condition, ordinalVariableProvider)
-        }
-        // If we're processing when comma-separated condition then body of the last branch should be comparison.
-        if (whenExpr.branches.isNotEmpty() && whenExpr.branches.last() is IrConst<*>) {
-            val lastBranch  = whenExpr.branches.last()
-            lastBranch.result = lowerIfNeeded(lastBranch.result, ordinalVariableProvider)
-        }
-    }
+    private fun isCommaSeparatedCondition(condition: IrExpression): Boolean =
+            condition is IrWhen && condition.origin == IrStatementOrigin.WHEN_COMMA
 
-    private fun lowerIfNeeded(expression: IrExpression, ordinalVariableProvider: () -> IrVariable) =
-        if (isComparisonWithEnumEntry(expression)) {
+    private fun lowerWhen(whenExpr: IrWhen, ordinalVariableProvider: () -> IrVariable) {
+
+        fun lowerExpressionIfNeeded(expression: IrExpression) = if (isComparisonWithEnumEntry(expression)) {
             createComparisonOfOrdinals(expression, ordinalVariableProvider)
         } else {
             expression
         }
+
+        whenExpr.branches.forEach {
+            it.condition = lowerExpressionIfNeeded(it.condition)
+        }
+        // If we're processing when comma-separated condition then body of the last branch should be comparison.
+        if (whenExpr.branches.isNotEmpty() && whenExpr.branches.last() is IrConst<*>) {
+            val lastBranch  = whenExpr.branches.last()
+            lastBranch.result = lowerExpressionIfNeeded(lastBranch.result)
+        }
+    }
 
     private fun createComparisonOfOrdinals(expression: IrExpression, ordinalVariableProvider: () -> IrVariable): IrCall {
         // Checked before in isComparisonWithEnumEntry
@@ -115,14 +114,9 @@ internal class EnumWhenLowering(
         }
     }
 
-    private fun isCommaSeparatedCondition(branch: IrBranch): Boolean {
-        val condition = branch.condition
-        return condition is IrWhen && condition.origin == IrStatementOrigin.WHEN_COMMA
-    }
-
     // We are looking for branch that is a comparison of the subject and another enum entry.
     // Both entries should belong to the same class.
-    private fun isComparisonWithEnumEntry(expression : IrExpression): Boolean {
+    private fun isComparisonWithEnumEntry(expression: IrExpression): Boolean {
         val call = expression as? IrMemberAccessExpression
                 ?: return false
         if (call.origin != IrStatementOrigin.EQEQ) {
@@ -137,7 +131,7 @@ internal class EnumWhenLowering(
         return false
     }
 
-    private fun createOrdinalVariable(irBlock: IrBlock): IrVariable {
+    private fun createEnumOrdinalVariable(irBlock: IrBlock): IrVariable {
         val tempVariable = irBlock.statements[0] as IrVariable
         val ordinalPropertyGetter = context.ir.symbols.enum.getPropertyGetter("ordinal")!!
         val getOrdinal = IrCallImpl(tempVariable.startOffset, tempVariable.endOffset, ordinalPropertyGetter).apply {
